a46115e33445fdace55047e344ad778d0cd50ecd
swatijarwal <swatijarwal365@gmail.com>
2023-12-21 18:35:31 +0530

    done

---
+++ projectORS/src/main/java/com/rays/dao/SubjectDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.SubjectDTO;
+/**
+ * @author swati jarwal
+ *
+ */
+public interface SubjectDAOInt extends BaseDAOInt<SubjectDTO>{
+
+}
